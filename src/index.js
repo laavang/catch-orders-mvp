a48ca7c8f7b9ce1b79604481d208bad9a5d5aef9
laavang <laavang@gmail.com>
2020-12-01 20:37:20 -0600

    Reverted index.js file to end of day yesterday

--- src/index.js
+++ src/index.js
@@ -7,16 +7,13 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const audience = process.env.REACT_APP_BACKEND_URI;
-
-console.log(domain, clientId);
+// const dashboardUri = process.env.REACT_APP_DASHBOARD_URI;
 
 ReactDOM.render(
     <Auth0Provider
         domain={domain}
         clientId={clientId}
         redirectUri = {window.location.origin}
-        audience={audience}
     >
     <App />
     </Auth0Provider>,
