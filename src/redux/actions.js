31c9b92ab99563386e75d47b9fc4f8b9878c7955
laavang <laavang@gmail.com>
2020-12-03 19:10:17 -0600

    Placed window reload from last commit in .then() chain

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -216,10 +216,10 @@ export const processLicense = (license) => {
                 body: JSON.stringify(licenseToProcess)
             })
             .then(dispatch(processLicenseSuccess(license)))
+            .then(window.location.reload();)
             .catch(e => {
                 console.log('License processing error ===> ', e);
             });
-            window.location.reload();
 }
 }
 
